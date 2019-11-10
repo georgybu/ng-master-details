f84c9e5d226155862552b3f2bf7290a11ea487be
Georgy Bunin <georgyb@mprest.com>
2019-11-10 08:02:16 +0200

    fix pagination

--- src/app/products/components/pagination.component.ts
+++ src/app/products/components/pagination.component.ts
@@ -7,7 +7,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
             <li *ngIf="currentPage > 0" (click)="pageChange.emit(currentPage - 1)">prev</li>
             <li *ngFor="let page of range; let i = index"
                 [ngClass]="{ 'current': currentPage === i}" (click)="pageChange.emit(i)">
-                {{ i  }}
+                {{ i + 1 }}
             </li>
             <li *ngIf="currentPage + 1 < pagesCount" (click)="pageChange.emit(currentPage + 1)">next</li>
         </ul>
